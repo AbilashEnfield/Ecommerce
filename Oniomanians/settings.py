345ebd9e0ad34df8c8d5efb56cbf414a9c97968b
Abilash <abilashenfield@gmail.com>
2021-04-20 10:42:36 +0530

    sixth hosting

--- Oniomanians/settings.py
+++ Oniomanians/settings.py
@@ -57,8 +57,7 @@ ROOT_URLCONF = 'Oniomanians.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'DIRS': [BASE_DIR + "/templates", ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
